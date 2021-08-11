1b3605e2a583651130e670520277461153e90d35
Arman Nakhwa <armannakhwa19@gmail.com>
2021-08-11 15:37:50 +0530

    11/08/2021

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './style.css';
 import {Header} from'./Comp/Header';
-import React from "react"
+import React from "react";
+import logo from './giphy.gif';
 
 function App() {
   return (
 <span>
-
 <h1>IP LAB React JS Practice Code Here</h1>
 <div className="Container">
+  <img src={logo} width="50%" alt="Error" />
 <Header/>
 </div>
 </span>
