61b6da2543c27c137637bfe1ea46b3260082c09f
Arman Nakhwa <armannakhwa19@gmail.com>
2021-08-11 17:30:26 +0530

    11/08/2021

--- src/Comp/Header.js
+++ src/Comp/Header.js
@@ -11,13 +11,14 @@ function count()
     s=s+1;
 
 }
-    return(
-   
+
+return(
 <>
 <h2>Hello Te Students</h2>
 <button onClick={count}>Counter</button>
 <b className="count">{a}</b>
 </>
-    );
+);
+
 
 }
