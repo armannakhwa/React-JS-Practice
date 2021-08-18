35890ad9ffa6e4b12e6ad37829f12bd88dcb2163
Arman Nakhwa <armannakhwa19@gmail.com>
2021-08-18 12:37:55 +0530

    18_08_2021 BOOSTRAP navbar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './style.css';
 import {Header} from'./Comp/Header';
-import React from "react";
+import React,{useState} from "react";
 import logo from './giphy.gif';
 import Propeg from "./Comp/Propeg";
 import Timing from "./Comp/Timing";
@@ -11,20 +11,27 @@ import {
   Route
 } from "react-router-dom";
 
+var s=0;
 function HomeScreen()
 {
+  const [a,seta]=useState();
+
+function count()
+{
+    seta(s);
+    s=s+1;
+
+}
   return(
     <>
-<h1>IP LAB React JS Practice Code Here
-<a href="https://github.com/armannakhwa/React-JS-Practice"><button style={{backgroundColor:'gold',color:'blue'}}>view entire code of this website</button></a>
-</h1>
-
+<div className="v-h-center">
 <div className="Container">
   <img src={logo} width="50%" alt="Error" />
-<Header/>
 <br/>
-
+<button onClick={count}>Counter</button>
+<b className="count">{a}</b><br/>
 <div>Designed By Arman</div>
+</div>
 </div>
 </>
   );
@@ -36,6 +43,7 @@ function App() {
  
   
 <Router>
+<Header/>
 <Switch>
           <Route exact path="/">
           <HomeScreen/>
--- src/Comp/Header.js
+++ src/Comp/Header.js
@@ -1,27 +1,46 @@
-import React, { useState } from "react";
+import React from "react";
 import {
     Link
   } from "react-router-dom";
-var s=0;
+
 export function Header()
 {
-
-const [a,seta]=useState();
-
-function count()
-{
-    seta(s);
-    s=s+1;
-
-}
-
 return(
 <>
-<h2>Hello Te Students</h2>
-<button onClick={count}>Counter</button>
-<b className="count">{a}</b><br/>
-<Link to="/16_08_2021"><button>view work 16_08_2021</button></Link>
-<Link to="/17_08_2021"><button>view work 17_08_2021</button></Link>
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">React-Js Practice Code Here</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <a href="https://github.com/armannakhwa/React-JS-Practice" className="nav-link" style={{color:'white'}}>view entire code of this website</a>     
+             </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Works
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+          <li><Link className="dropdown-item" to="/">Specific Example's</Link></li>
+          <li><hr className="dropdown-divider"/></li>
+            <li><Link className="dropdown-item" to="/16_08_2021">view work 16_08_2021</Link></li>
+            <li><Link className="dropdown-item" to="/17_08_2021">view work 17_08_2021</Link></li>
+          </ul>
+        </li>
+      
+      </ul>
+      {/* <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-light" type="submit">Search</button>
+      </form> */}
+    </div>
+  </div>
+</nav>
 </>
 );
 
